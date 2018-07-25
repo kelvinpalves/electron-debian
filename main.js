6cb94a07fdb2f9e8e7389292455406a9a025b8a4
Kelvin Alves <kelvinpalves@gmail.com>
2018-07-25 09:33:49 -0300

    Versão funcionando da porta serial e versão do electron

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ let win;
 function createWindow () {
     win = new BrowserWindow({ width: 800, height: 600 });
     win.loadFile('index.html');
+    win.webContents.openDevTools()
 
     win.on('closed', () => {
         win = null;
